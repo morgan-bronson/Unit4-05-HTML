b4268881cb936a29fb7dd51850310a182cfc684f
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-10 03:53:30 +0000

    fixed errors

--- script.js
+++ script.js
@@ -3,11 +3,9 @@
 document.getElementById('mybtn').addEventListener('click', calculatearea)
 
 function calculatearea () {
-  let length  = 0 
+  let length = 0
   let area = 0
-  
   length = document.getElementById('length').value
   area = parseFloat(length) * parseFloat(length)
-  
   alert(area)
 }
